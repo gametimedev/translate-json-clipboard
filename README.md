ff402b624c4d3f9a244e2da9e7ea3a1a81fad643
GameTimeDev <62714641+gametimedev@users.noreply.github.com>
2024-11-03 22:19:09 +0100

    Replace

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # A little helper for copy & past
 This extension should make it easier to quickly copy the path for a STRING value of a JSON file.
 
-## How to use
+## How to use | Copy & Paste
 Select any key in your json (which has a STRING as value) and press one of the three keyboard shortcuts.
 ![alt text](https://data.gametimedev.de/other/translate-json-clipboard/example.png)
 
@@ -21,7 +21,10 @@ Shortcut: <kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>Y</kbd><br>
 Format: **PATH**<br>
 Format: **EXAMPLE.OTHER.TEST**
 
+## How to use | Replace
+Mark a section in a HTML or TS file and press <kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>A</kbd>. The extension will search for an exact match in your main transfer file and insert the first match.
 
 ## Limitations
 - Works only for STRING values
 - If Key and String has multible it will always select the first (even if selected the other one)
+- Some problems with special values like \n \r \"...
--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,9 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
-
-	console.log("translate-json-clipboard is active!");
+    handleMainFile(context);
 
     let c1 = vscode.commands.registerCommand('translate-json-clipboard.html-path', () => {
         const editor = vscode.window.activeTextEditor;
@@ -13,7 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
             const line = document.lineAt(selection.active.line);
             const lineText = line.text;
             let items = getJsonPath(document.getText(),text,lineText);
-            if(items.length==0){
+            if(items.length===0){
                 vscode.window.showErrorMessage("Unable to find key path!");
                 return;
             }
@@ -36,7 +37,7 @@ export function activate(context: vscode.ExtensionContext) {
             const line = document.lineAt(selection.active.line);
             const lineText = line.text;
             let items = getJsonPath(document.getText(),text,lineText);
-            if(items.length==0){
+            if(items.length===0){
                 vscode.window.showErrorMessage("Unable to find key path!");
                 return;
             }
@@ -59,7 +60,7 @@ export function activate(context: vscode.ExtensionContext) {
             const line = document.lineAt(selection.active.line);
             const lineText = line.text;
             let items = getJsonPath(document.getText(),text,lineText);
-            if(items.length==0){
+            if(items.length===0){
                 vscode.window.showErrorMessage("Unable to find key path!");
                 return;
             }
@@ -76,6 +77,83 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(c1);
 	context.subscriptions.push(c2);
     context.subscriptions.push(c3);
+}
+
+function handleMainFile(context: vscode.ExtensionContext){
+    const config = vscode.workspace.getConfiguration('translatejsonclipboard');
+    const mainFile = config.get('mainTranslateFile');
+    const fileName = config.get('mainTranslateFileName');
+
+    let filepath : string | undefined = undefined;
+    if(typeof mainFile === "string" && mainFile.trim().length>0 && fs.existsSync(mainFile)){
+        filepath = mainFile;
+    }else if(typeof mainFile === "string" && mainFile.trim().length>0 && !fs.existsSync(mainFile)){
+        vscode.window.showErrorMessage("Invalid path to translation file!");
+        config.update('mainTranslateFile','');
+    }
+    if(!filepath && typeof fileName === "string" && fileName.trim().length>0){
+        filepath = searchMainFile(fileName);
+        if(filepath && fs.existsSync(filepath)){
+            config.update('mainTranslateFile',filepath);
+            vscode.window.showInformationMessage("Translation file path was updated\n"+filepath);
+        }
+    }
+
+    let c4 = vscode.commands.registerCommand('translate-json-clipboard.file-replace', () => {
+        const editor = vscode.window.activeTextEditor;
+
+        if(!filepath || !fs.existsSync(filepath)){
+            vscode.window.showErrorMessage("Invalid path to translation file!");
+            return;
+        }
+
+        let file = JSON.parse(fs.readFileSync(filepath).toString());
+
+        if (file && editor && (editor.document.fileName.endsWith('.html') || editor.document.fileName.endsWith('.ts'))) {
+            const document = editor.document;
+            const selection = editor.selection;
+            const text = document.getText(selection);
+            let items = iterateJsonFindeValue(file,[],text);
+            if(items.length===0){
+                vscode.window.showErrorMessage("No matching value was found!");
+                return;
+            }
+            let replace = items[0].path;
+            if(editor.document.fileName.endsWith('.html')){replace = `{{'${items[0].path}' | translate}}`;}
+            if(editor.document.fileName.endsWith('.ts')){replace = `this.translate.instant('${items[0].path}')`;}
+            const start = selection.start;
+            const end = selection.end;
+            const before = start.character > 0 ? document.getText(new vscode.Range(start.translate(0, -1), start)) : '';
+            const after = end.character < document.lineAt(end.line).text.length ? document.getText(new vscode.Range(end, end.translate(0, 1))) : '';
+            const isQuoteBefore = before === '"' || before === "'";
+            const isQuoteAfter = after === '"' || after === "'";
+            const newStart = isQuoteBefore ? start.translate(0, -1) : start;
+            const newEnd = isQuoteAfter ? end.translate(0, 1) : end;
+            const newRange = new vscode.Range(newStart, newEnd);
+            editor.edit(editBuilder => {
+                editBuilder.replace(newRange, replace);
+            });
+        }
+    });
+
+    context.subscriptions.push(c4);
+
+
+}
+
+function searchMainFile(filename:string):undefined | string{
+    vscode.window.showInformationMessage("Started auto search for en.json file...");
+    const projectPath = getCurrentWorkspacePath();
+    if(!projectPath){return undefined;}
+    return findFileSync(projectPath,filename);
+}
+
+function getCurrentWorkspacePath(): string | undefined {
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (workspaceFolders && workspaceFolders.length > 0) {
+        return workspaceFolders[0].uri.fsPath;
+    }
+    return undefined;
 }
 
 function getJsonPath(jsonBody:string,key:string,line:string):any[]{
@@ -97,17 +175,67 @@ function iterateJson(obj: any, path: string[] = [],searchKey:string,serachValue:
             if (typeof obj[key] === 'object' && obj[key] !== null) {
                 let de = iterateJson(obj[key], newPath,searchKey,serachValue);
                 items.push(... de);
-            } else if(key==searchKey && obj[key]==serachValue) {
-                let item = {
-                    value:obj[key],
-                    path:newPath.join('.'),
-                    key:key
+            } else{
+                let normalizedKey = obj[key].toString().replace(/\r\n/g, '\n');
+                let normalizedSearchValue = serachValue.replace(/\r\n/g, '\n');
+                normalizedKey = normalizedKey.replace(/\"/g, "\\\"");
+                normalizedKey = normalizedKey.replace(/\n/g, "\\n");
+                if(key===searchKey && normalizedKey === normalizedSearchValue) {
+                    let item = {
+                        value:obj[key],
+                        path:newPath.join('.'),
+                        key:key
+                    };
+                    items.push(item);
                 }
-                items.push(item);
             }
         }
     }
     return items;
+}
+
+function iterateJsonFindeValue(obj: any, path: string[] = [],serachValue:string):any[] {
+    let items : any[] = [];
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const newPath = path.concat(key);
+            if (typeof obj[key] === 'object' && obj[key] !== null) {
+                let de = iterateJsonFindeValue(obj[key], newPath,serachValue);
+                items.push(... de);
+            } else{
+                let normalizedKey = obj[key].toString().replace(/\r\n/g, '\n');
+                let normalizedSearchValue = serachValue.replace(/\r\n/g, '\n');
+                normalizedKey = normalizedKey.replace(/\"/g, "\\\"");
+                normalizedKey = normalizedKey.replace(/\n/g, "\\n");
+                if(normalizedKey === normalizedSearchValue) {
+                    let item = {
+                        value:obj[key],
+                        path:newPath.join('.'),
+                        key:key
+                    };
+                    items.push(item);
+                }
+            }
+        }
+    }
+    return items;
+}
+
+function findFileSync(directory: string, fileName: string): string | undefined {
+    const files = fs.readdirSync(directory, { withFileTypes: true });
+    for (const file of files) {
+        const fullPath = path.join(directory, file.name);
+
+        if (file.isDirectory()) {
+            const result = findFileSync(fullPath, fileName);
+            if (result) {
+                return result;
+            }
+        } else if (file.name === fileName) {
+            return fullPath;
+        }
+    }
+    return undefined;
 }
 
 export function deactivate() {}
